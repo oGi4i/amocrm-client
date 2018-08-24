001d0a229f3871427b25e746ab09697d4849e87d
javdetserg <javdetserg@gmail.com>
2018-08-25 01:14:45 +0500

    test11

--- client.go
+++ client.go
@@ -129,7 +129,7 @@ func (c *clientInfo) DoGet(url string, data map[string]string) (io.Reader, error
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(resp.Status)
+	fmt.Println(ioutil.ReadAll(resp.Body))
 	fmt.Println(resp.Body)
 	defer resp.Body.Close()
 	return resp.Body, nil
