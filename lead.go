014cf9556604a675b5d6c601300e9c026936b0c8
javdetserg <javdetserg@gmail.com>
2018-11-08 23:55:21 +0500

    addlead

---
+++ lead.go
@@ -0,0 +1,18 @@
+package amocrm
+
+import "errors"
+
+func (c *clientInfo) AddLead(lead Lead) (int, error) {
+	if lead.Name == "" {
+		return 0, errors.New("Name is empty")
+	}
+	if lead.StatusID == "" {
+		return 0, errors.New("StatusID is empty")
+	}
+	url := c.Url + apiUrls["leads"]
+	resp, err := c.DoPost(url, LeadSetRequest{Add: []Lead{lead}})
+	if err != nil {
+		return 0, err
+	}
+	return c.GetResponseID(resp)
+}
